8c6e86bf422ea5e41fe06c38125d66e60a7402eb
Armaan A <armaanaggarwal@gmail.com>
2024-02-27 01:58:26 -0800

    configure example

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
-import * as ExpoSuperwall from "expo-superwall";
+import * as Superwall from "expo-superwall";
+import { useEffect } from "react";
+
+const SUPERWALL_API_KEY = process.env.EXPO_PUBLIC_SUPERWALL_API_KEY;
+const REVENUECAT_API_KEY = process.env.EXPO_PUBLIC_REVENUECAT_API_KEY;
+
+if (!SUPERWALL_API_KEY || !REVENUECAT_API_KEY) {
+  throw new Error("Please setup required environment variables");
+}
+
+Superwall.configure(SUPERWALL_API_KEY);
+Superwall.syncSubscriptionStatus(REVENUECAT_API_KEY);
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  useEffect(() => {
+    // Make a campaign in Superwall that when the "View Home Screen" event gets triggered, it shows a paywall
+    Superwall.register("View Home Screen");
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
